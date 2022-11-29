fa53dec6f4167b09cf2249b8486f30b8339da60b
playaz44 <playaz44@users.noreply.github.com>
2022-11-29 17:38:23 +0100

    Update MessageBuilder.php

--- php/MessageBuilder.php
+++ php/MessageBuilder.php
@@ -182,7 +182,7 @@ class MessageBuilder
     public function getProtocolMessage()
     {
 	$formatLen = strlen($this->format ?? '');
-    
+
         return pack('VVVV',
                     strlen($this->html)+1,
                     strlen($this->text)+1,
